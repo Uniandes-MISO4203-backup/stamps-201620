a5f47834df79a0a2883b5a456e23ef48727438e5
ga.chica10 <ga.chica10@MISO4203-8.virtual.uniandes.edu.co>
2016-08-27 22:29:58 -0500

    Merge origin/master
    Conflicts:
    	stamps-api/src/main/java/co/edu/uniandes/csw/stamps/dtos/minimum/StampDTO.java
    	stamps-api/src/main/webapp/src/modules/stamp/instance/detail/stamp.detail.tpl.html
    	stamps-api/src/main/webapp/src/modules/stamp/list/stamp.gallery.tpl.html
    	stamps-api/src/main/webapp/src/modules/stamp/stamp.mod.js
    	stamps-logic/src/main/java/co/edu/uniandes/csw/stamps/entities/StampEntity.java

--- stamps-logic/src/main/java/co/edu/uniandes/csw/stamps/entities/StampEntity.java
+++ stamps-logic/src/main/java/co/edu/uniandes/csw/stamps/entities/StampEntity.java
@@ -38,9 +38,11 @@ import java.util.ArrayList;
 @Entity
 public class StampEntity extends BaseEntity implements Serializable {
 
+    private Long id;
+    private String name;
     private String image;
-    private boolean availableForSale;
     private Long price;
+    private boolean availableForSale;
     private short qualification;
     private String description;
 
@@ -50,7 +52,46 @@ public class StampEntity extends BaseEntity implements Serializable {
 
     @PodamExclude
     @ManyToMany
-    private List<CategoryEntity> category = new ArrayList<>();
+    private List<CategoryEntity> category = new ArrayList<>(); 
+    
+    /**
+     * Obtiene el atributo id.
+     *
+     * @return atributo id.
+     * @generated
+     */
+    public Long getId() {
+        return id;
+    }
+    /**
+     * Establece el valor del atributo id.
+     *
+     * @param id nuevo valor del atributo
+     * @generated
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    /**
+     * Obtiene el atributo name.
+     *
+     * @return atributo name.
+     * @generated
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Establece el valor del atributo name.
+     *
+     * @param name nuevo valor del atributo
+     * @generated
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
     /**
      * Obtiene el atributo image.
