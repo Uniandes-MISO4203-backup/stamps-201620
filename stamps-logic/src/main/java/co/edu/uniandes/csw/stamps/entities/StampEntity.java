e9f17575cb24e9aeeb533e09887393a0cde0f41b
de.paniagua <de.paniagua>
2016-08-29 23:26:12 -0500

    Revert "Merge origin/master"
    This reverts commit a5f47834df79a0a2883b5a456e23ef48727438e5.

--- stamps-logic/src/main/java/co/edu/uniandes/csw/stamps/entities/StampEntity.java
+++ stamps-logic/src/main/java/co/edu/uniandes/csw/stamps/entities/StampEntity.java
@@ -38,11 +38,9 @@ import java.util.ArrayList;
 @Entity
 public class StampEntity extends BaseEntity implements Serializable {
 
-    private Long id;
-    private String name;
     private String image;
-    private Long price;
     private boolean availableForSale;
+    private Long price;
     private short qualification;
     private String description;
 
@@ -52,46 +50,7 @@ public class StampEntity extends BaseEntity implements Serializable {
 
     @PodamExclude
     @ManyToMany
-    private List<CategoryEntity> category = new ArrayList<>(); 
-    
-    /**
-     * Obtiene el atributo id.
-     *
-     * @return atributo id.
-     * @generated
-     */
-    public Long getId() {
-        return id;
-    }
-    /**
-     * Establece el valor del atributo id.
-     *
-     * @param id nuevo valor del atributo
-     * @generated
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
-    
-    /**
-     * Obtiene el atributo name.
-     *
-     * @return atributo name.
-     * @generated
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Establece el valor del atributo name.
-     *
-     * @param name nuevo valor del atributo
-     * @generated
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+    private List<CategoryEntity> category = new ArrayList<>();
 
     /**
      * Obtiene el atributo image.
