503fc6251e192315488bb65dadab841d1a8092c6
ga.chica10 <ga.chica10@MISO4203-8.virtual.uniandes.edu.co>
2016-09-19 22:15:19 -0500

    Cambios entity de artista

--- stamps-logic/src/main/java/co/edu/uniandes/csw/stamps/entities/ArtistEntity.java
+++ stamps-logic/src/main/java/co/edu/uniandes/csw/stamps/entities/ArtistEntity.java
@@ -38,8 +38,6 @@ import javax.persistence.CascadeType;
 @Entity
 public class ArtistEntity extends BaseEntity implements Serializable {
 
-    private Long id;
-    private String name;
     private String address;
     private Long telephone;
     private short qualification;
@@ -67,22 +65,7 @@ public class ArtistEntity extends BaseEntity implements Serializable {
      */
     public void setStamps(List<StampEntity> stamps) {
         this.stamps = stamps;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getAddress() {
