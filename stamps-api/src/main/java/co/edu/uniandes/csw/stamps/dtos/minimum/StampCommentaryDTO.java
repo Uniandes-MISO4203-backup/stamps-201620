c55859a4a151dd0e9188f858b28defce413e998c
ga.chica10 <ga.chica10@MISO4203-8.virtual.uniandes.edu.co>
2016-10-23 16:39:54 -0500

    Creacion StampCommentaryDTO

---
+++ stamps-api/src/main/java/co/edu/uniandes/csw/stamps/dtos/minimum/StampCommentaryDTO.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.stamps.dtos.minimum;
+
+import co.edu.uniandes.csw.stamps.entities.StampCommentaryEntity;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author ga.chica10
+ */
+public class StampCommentaryDTO implements Serializable{
+    
+    private Long id;
+    private String name;
+    private String commentary;
+    private short qualification;
+    private Date DateCommentary;
+
+    /**
+     * Crea un objeto StampCommentaryMinimumDTO a partir de un objeto StampCommentaryEntity.
+     *
+     * @param entity Entidad StampCommentaryEntity desde la cual se va a crear el nuevo objeto.
+     * @generated
+     */
+    
+    public StampCommentaryDTO(StampCommentaryEntity entity) {
+        if (entity!=null){
+            this.id = entity.getId();
+            this.name = entity.getName();
+            this.commentary = entity.getCommentary();
+            this.qualification = entity.getQualification();
+            this.DateCommentary = entity.getDateCommentary();
+        }
+    }
+    
+    /**
+     * Convierte un objeto StampCommentaryMinimumDTO a StampCommentaryEntity.
+     *
+     * @return Nueva objeto StampCommentaryEntity.
+     * @generated
+     */
+    
+    public StampCommentaryEntity toEntity() {
+        StampCommentaryEntity entity = new StampCommentaryEntity();
+        entity.setId(this.getId());
+        entity.setName(this.getName());
+        entity.setCommentary(this.getCommentary());
+        entity.setQualification(this.getQualification());
+        entity.setDateCommentary(this.DateCommentary);
+    return entity;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCommentary() {
+        return commentary;
+    }
+
+    public void setCommentary(String commentary) {
+        this.commentary = commentary;
+    }
+
+    public short getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(short qualification) {
+        this.qualification = qualification;
+    }
+
+    public Date getDateCommentary() {
+        return DateCommentary;
+    }
+
+    public void setDateCommentary(Date DateCommentary) {
+        this.DateCommentary = DateCommentary;
+    }
+}
